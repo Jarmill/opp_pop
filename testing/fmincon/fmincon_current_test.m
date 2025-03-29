4dde53d91179289979fb03eea656dab821da75e9
Jared Miller <jared.fr.miller@gmail.com>
2025-03-29 17:30:13 +0100

    fmincon can produce opp when seeded at starting patterns
    at least one point. try more!

--- testing/fmincon/fmincon_current_test.m
+++ testing/fmincon/fmincon_current_test.m
@@ -15,7 +15,14 @@ af = [0; a; pi/2];
 da = diff(af);
 I0 = sdpvar(1, 1);
 
-%form the energy
+%start with an initial point (from the recovered pattern)
+I0_start = osc.pattern.I(1);
+alpha_start = osc.pattern.alpha(1:d)';
+af0 = [0; alpha_start; pi/2];
+assign(a, alpha_start);
+assign(I0, I0_start);
+
+%% form the energy objective
 I = cumsum([0; da.*u])+I0;
 E = zeros(length(I)-1, 1, 'like', sdpvar);
 
@@ -29,11 +36,46 @@ end
 E_all = 4*sum(E);
 objective = E_all;
 
+%% form the harmonics constraints
+sinind = osc.opts.harmonics.index_sin;
+b = zeros(length(sinind), 1, 'like', sdpvar);
+b0 = zeros(length(sinind), 1); %for sanity checks
+for l  = 1:length(sinind)
+    lc = sinind(l);
+    for m = 1:d        
+        % costerm =  (u(m)/lc)*(cos(lc*af(m+1)) - cos(lc*af(m)))
+        % b(l) = b(l) + costerm;
+        chopblock = (u/lc) .* (-cos(lc*af(2:end)) + cos(lc*af(1:end-1)));
+        b(l) = 4*sum(chopblock)/pi;
 
-I0_start = osc.pattern.I(1);
-alpha_start = osc.pattern.alpha(1:d)';
+        chopblock0 = (u/lc) .* (-cos(lc*af0(2:end)) + cos(lc*af0(1:end-1)));
+        b0(l) = 4*sum(chopblock0)/pi;
+    end
+end
+harm_tol = 1e-5;
+con_harm = harmonics_process(osc.opts.harmonics, [zeros(size(osc.opts.harmonics.index_cos)); b], harm_tol);
 
-E_start = replace(E_all, [I0; a], [I0_start; alpha_start])
+%% form the ordering constraints
+Theta = osc.opts.f0*osc.opts.Ts*2*pi;
+Theta_lim = Theta*[ones(d, 1); 0.5];
+adiff = (af(2:end) - af(1:end-1) + Theta_lim);
+con_order = adiff>=0;
 
-cons = [];
+E_start = replace(E_all, [I0; a], [I0_start; alpha_start]);
 
+
+%% assemble and solve
+% cons = [con_harm; con_order];
+
+% cons = [con_harm; con_order; I<=0; diff(I) <= 0];
+cons = [con_harm; con_order; I<=0];
+
+sdpopts = sdpsettings('solver', 'fmincon', 'usex0',1);
+sol = optimize(cons, objective, sdpopts);
+
+if sol.problem ==0
+    a_rec = value(a);
+    I_rec = value(I);    
+    E_rec = value(objective);
+    b_rec = value(b);
+end
